e49f8602fa6bd55b4cba6ad7872ee4b4e5d4996f
favaz0 <favaz@sayonetech.com>
2023-11-21 21:59:55 +0530

    basic security with saved password

---
+++ src/main/java/com/app/kuri/Config/SecurityConfig.java
@@ -0,0 +1,24 @@
+package com.app.kuri.Config;
+
+import static org.springframework.security.config.Customizer.withDefaults;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.web.SecurityFilterChain;
+
+@Configuration
+public class SecurityConfig {
+    
+    @Bean
+    public SecurityFilterChain filterChain(HttpSecurity http) throws Exception{
+        http.authorizeRequests(
+            auth -> auth.anyRequest().authenticated()
+        );
+
+        http.httpBasic(withDefaults());
+
+        http.csrf().disable();
+        
+        return http.build();
+    }
+}
